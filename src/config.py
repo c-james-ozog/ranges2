896307827a7f5bb7fb6fcc3a27ad9321cbb176fb
c-james-ozog <100250390+c-james-ozog@users.noreply.github.com>
2026-04-09 16:20:32 -0500

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,14 @@
+CONTRACTS = [
+    {"commodity": "Corn", "symbol": "ZCN26.CBT", "base_symbol": "ZCN26", "month": "Jul"},
+    {"commodity": "Corn", "symbol": "ZCZ26.CBT", "base_symbol": "ZCZ26", "month": "Dec"},
+    {"commodity": "Crude Oil WTI", "symbol": "CLM26.NYM", "base_symbol": "CLM26", "month": "Jun"},
+    {"commodity": "Gold", "symbol": "GCJ26.CMX", "base_symbol": "GCJ26", "month": "Apr"},
+    {"commodity": "S&P 500 E-Mini", "symbol": "ESM26.CME", "base_symbol": "ESM26", "month": "Jun"}
+]
+
+TICK_SIZES = {
+    "Corn": 0.25,
+    "Crude Oil WTI": 0.01,
+    "Gold": 0.1,
+    "S&P 500 E-Mini": 0.25
+}
